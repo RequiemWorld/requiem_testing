49974cb4c11193982c7afdbffc50f3040557633c
polllaris <polllaris@protonmail.com>
2025-05-31 21:11:24 -0400

    add function to assert that a string ends with another string and give a more helpful message

---
+++ src/requiem_testing/experimental/helper/helpful_asserts.py
@@ -0,0 +1,15 @@
+
+
+def assert_string_ends_with(string: str, ending: str, msg: str | None = None) -> None:
+    """
+    Asserts that a string ends with another string, raising
+    an assertion error that says what the actual string is that doesn't end with it.
+
+    :raises AssertionError: if the given string doesn't end with the given ending.
+    """
+    if msg is None:
+        assertion_message = f"ending {ending} not found in {msg}"
+    else:
+        assertion_message = msg
+
+    assert string.endswith(ending), assertion_message
